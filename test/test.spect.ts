941604a6d16a3d6c87f7e7288e71dc6c7b4fd086
Ainoa Iglesias <alu0101164403@ull.edu.es>
2022-03-29 17:55:30 +0000

    añadido test suma para comprobacion

--- test/test.spect.ts
+++ test/test.spect.ts
@@ -1,12 +1,10 @@
 import 'mocha';
-/*
 import {expect} from 'chai';
+import {add} from '../src/index';
 
-describe('Test ', () => {
-  describe('Test ', () => {
-    it('', () => {
-      expect().to.be.equal();
-    });
+describe('Test index', () => {
+  it('Se compruba la suma de 1+7', () => {
+    expect(add(1, 7)).to.be.equal(8);
   });
 });
-*/
+
