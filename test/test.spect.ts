8f4a838d622974601979e3ca9204a0c3c98c6133
Ainoa Iglesias <alu0101164403@ull.edu.es>
2022-03-29 17:18:28 +0000

    actualizado directorio test

---
+++ test/test.spect.ts
@@ -0,0 +1,12 @@
+import 'mocha';
+/*
+import {expect} from 'chai';
+
+describe('Test ', () => {
+  describe('Test ', () => {
+    it('', () => {
+      expect().to.be.equal();
+    });
+  });
+});
+*/
