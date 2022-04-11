21bff5b06c361c5fea2163d2ce24796b7d09bfdc
Marc Carbonell <alu0101323282@ull.edu.es>
2022-04-11 12:07:43 +0000

    Pruebas GenreManager y PlaylistManager

--- test/Managers/GenresManager.spec.ts
+++ test/Managers/GenresManager.spec.ts
@@ -5,11 +5,9 @@ import {GenreManager} from '../../src/Managers/GenreManager';
 
 describe('Pruebas de la clase GenreManager', () => {
   let genreManager: GenreManager;
-  let pop: Genre;
   let rock: Genre;
   before(function() {
     genreManager = GenreManager.getGenreManager();
-    pop = genreManager.searchByName('Pop');
     rock = genreManager.searchByName('Rock');
   });
   it(`GenreManager.getGenreManager() is not equal null`, () => {
@@ -17,11 +15,11 @@ describe('Pruebas de la clase GenreManager', () => {
   });
   it(`genreManager.removeGenre(rock) removes rock from the genre collection`, () => {
     genreManager.remove(rock);
-    expect(genreManager.getCollection()).to.be.eql(new Set<Genre>([pop]));
+    expect(genreManager.searchByName('Rock')).to.be.equal(undefined);
   });
   it(`genreManager.addGenre(rock) adds rock to the genre collection`, () => {
     genreManager.add(rock);
-    expect(genreManager.getCollection()).to.be.eql(new Set<Genre>([pop, rock]));
+    expect(genreManager.searchByName('Rock')).to.be.equal(rock);
   });
   it(`genreManager.getGenreByName('Rock') returns rock`, () => {
     expect(genreManager.searchByName('Rock')).to.be.equal(rock);
--- test/Managers/PlaylistManager.spec.ts
+++ test/Managers/PlaylistManager.spec.ts
@@ -5,22 +5,20 @@ import {Playlist} from '../../src/Basics/Playlist';
 
 describe('Pruebas de la clase PlaylistManager', () => {
   let list: Playlist;
-  let lista: Playlist;
   let playlistManager: PlaylistManager = PlaylistManager.getPlaylistManager();
   before(function() {
     list = playlistManager.searchByName('MyPlaylist');
-    lista = playlistManager.searchByName('MiLista');
   });
   it(`PlaylistManager.getPlaylistManager() is not equal null`, () => {
     expect(PlaylistManager.getPlaylistManager()).not.to.be.equal(null);
   });
   it(`playlistManager.removePlaylist(list) removes list from the genre collection`, () => {
     playlistManager.remove(list);
-    expect(playlistManager.getCollection()).to.be.eql(new Set<Playlist>([lista]));
+    expect(playlistManager.searchByName('MyPlaylist')).to.be.equal(undefined);
   });
   it(`playlistManager.addPlaylist(list) adds list to the genre collection`, () => {
     playlistManager.add(list);
-    expect(playlistManager.getCollection()).to.be.eql(new Set<Playlist>([lista, list]));
+    expect(playlistManager.searchByName('MyPlaylist')).to.be.equal(list);
   });
   it(`playlistManager.getPlaylistByName('MyPlaylist') returns list`, () => {
     expect(playlistManager.searchByName('MyPlaylist')).to.be.equal(list);
